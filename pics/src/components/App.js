596933a8a8857ac66cd354eddbd4bfa1017731b2
ChangKeun <ckdrmsdk@gmail.com>
2021-04-19 09:51:33 +0900

    render image list from parent component

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 
 import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
 
 class App extends Component {
   state = { images: [] };
@@ -23,7 +24,7 @@ class App extends Component {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} images
+        <ImageList images={this.state.images} />
       </div>
     );
   }
--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,5 +1,15 @@
-const ImageList = () => {
-  return <div>Image List</div>;
+const ImageList = ({ images }) => {
+  const imageList = images.map(({ description, id, urls }) => {
+    return <img alt={description} key={id} src={urls.regular} />;
+  });
+
+  return <div>{imageList}</div>;
 };
 
 export default ImageList;
+
+//! Purpose of keys props
+// For perfomance reason
+
+// When React rerenders component, to compare and
+// render more efficiently with previous DOM.
