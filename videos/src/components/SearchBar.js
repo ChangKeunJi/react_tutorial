f00dcf5fe5fd966fd6bbc1ca2708ac460d557cc0
ChangKeun <ckdrmsdk@gmail.com>
2021-04-21 08:34:17 +0900

    Setting for youtube api project

---
+++ videos/src/components/SearchBar.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  state = { term: "" };
+
+  onInputChange = (e) => {
+    this.setState({ term: e.target.value });
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+
+    //TODO:  We call callback from parent
+  };
+
+  render() {
+    console.log(this.state.term);
+    return (
+      <div className="ui segment search-bar">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              value={this.state.term}
+              onChange={this.onInputChange}
+              type="text"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
