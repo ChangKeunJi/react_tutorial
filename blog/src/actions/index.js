3f2e1f0cfde47c94c6da4b1ad89c4eb47c53957a
ChangKeun <ckdrmsdk@gmail.com>
2021-05-06 15:52:37 +0900

    Redux thunk in deep

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,14 +1,9 @@
 import jsonplaceholder from "../api/jsonPlaceHolder";
 
-export const fetchPosts = async () => {
-  // dispatch is the same function in Redux
-
-  return async function (dispatch, getState) {
+export const fetchPosts = () => {
+  return async (dispatch, getState) => {
     const res = await jsonplaceholder.get("/posts");
 
-    return {
-      type: "FETCH_POSTS",
-      payload: res,
-    };
+    dispatch({ type: "FETCH_POSTS", payload: res });
   };
 };
--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
+import postsReducer from "./postsReducer";
+
 export default combineReducers({
-  dummy: () => "dummy",
+  posts: postsReducer,
 });
 
 // Fake reducer. Randome key & Dummy function
