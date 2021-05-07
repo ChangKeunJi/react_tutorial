109db398521a6aff79bf1707d24e972ab11e8c01
ChangKeun <ckdrmsdk@gmail.com>
2021-05-07 15:40:22 +0900

    Refactoring redux w lodash memoize

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,9 +1,26 @@
 import jsonplaceholder from "../api/jsonPlaceHolder";
+import _ from "lodash";
 
 export const fetchPosts = () => {
   return async (dispatch, getState) => {
     const res = await jsonplaceholder.get("/posts");
 
-    dispatch({ type: "FETCH_POSTS", payload: res });
+    dispatch({ type: "FETCH_POSTS", payload: res.data });
   };
 };
+
+export const fetchUser = (id) => {
+  return (dispatch, getState) => {
+    // const res = await jsonplaceholder.get(`/users/${id}`);
+
+    // dispatch({ type: "FETCH_USER", payload: res.data });
+
+    _fetchUser(id, dispatch);
+  };
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const res = await jsonplaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: res.data });
+});
--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,21 +1,47 @@
 import { Component } from "react";
 import { connect } from "react-redux";
+
 import { fetchPosts } from "../actions";
+import UserHeader from "./UserHeader";
 
 class PostList extends Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
 
+  renderList() {
+    return this.props.posts.map((post) => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+            <UserHeader userId={post.userId} />
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>Post List</div>;
+    // console.log(this.props.posts);
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
 
-// const mapStateToProps = (state) => {
-//   return state;
-// };
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts,
+  };
+};
 
-export default connect(null, {
+export default connect(mapStateToProps, {
   fetchPosts: fetchPosts,
 })(PostList);
+
+//! Why do we have two console log
+// All reducers get called one time before React App
+// At this initial time, Reducer returns a default state
